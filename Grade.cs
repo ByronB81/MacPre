7bec11e6cdcd997f6d537a9d989eb698d3f98b38
Byron <byronblocker@gmail.com>
2017-07-19 15:57:56 -0700

    add in running bool

--- Grade.cs
+++ Grade.cs
@@ -4,19 +4,30 @@ class Grade
 {
   static void Main()
   {
-    Console.WriteLine("What grade did you get?");
-    string grade = Console.ReadLine();
-    if (grade.Contains("A")|| grade.Contains("B"))
+    bool appRunning = true;
+    while (appRunning)
     {
-      Console.WriteLine("Well done!");
-    }
-    else if (grade.Contains("C") || (grade.Contains("D")))
-    {
-      Console.WriteLine("You could do better");
-    }
-    else
-    {
-      Console.WriteLine("That is unacceptable");
+      Console.WriteLine("What grade did you get?");
+      string grade = Console.ReadLine();
+      if (grade.Contains("A")|| grade.Contains("B"))
+      {
+        Console.WriteLine("Well done!");
+        appRunning = false;
+      }
+      else if (grade.Contains("C") || (grade.Contains("D")))
+      {
+        Console.WriteLine("You could do better");
+        appRunning = false;
+      }
+      else if (grade.Contains("F"))
+      {
+        Console.WriteLine("You have failed.");
+        appRunning = false;
+      }
+      else
+      {
+        Console.WriteLine("That answer is no recognized. Please enter A, B, C, D, E, or F.");
+      }
     }
   }
 }
